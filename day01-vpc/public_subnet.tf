684b99016c0c6eb2061905a58fe09c2833cf5f1a
fabiomartinsbrrj <fabiomartinsbr@gmail.com>
2024-09-07 22:34:24 -0300

    feat: creating route table

--- day01-vpc/public_subnet.tf
+++ day01-vpc/public_subnet.tf
@@ -27,3 +27,11 @@ resource "aws_subnet" "public_subnet_1c" {
     Name = format("%s-public-subnet-1c", var.project_name)
   }
 }
+
+resource "aws_route_table" "public_internet_access" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = format("%s-public", var.project_name)
+  }
+}
